22f718a9f08bc93957cf520c571a2593042ac95a
unknown <xingguang64@yeah.net>
2024-10-08 15:27:07 +0800

    播放数量细节处理

--- src/components/playlist/index.ts
+++ src/components/playlist/index.ts
@@ -1,4 +1,4 @@
-import { defineComponent } from "@vue-mini/core";
+import { defineComponent, computed } from "@vue-mini/core";
 
 defineComponent({
   properties: {
@@ -7,6 +7,31 @@ defineComponent({
     },
   },
   setup(props) {
-    console.log(props.playlist);
+    // 小数转化，保留两位小数
+    const playCount = computed(() => {
+      const num = props.playlist.playCount as number;
+      const numStr = num.toString().split(".")[0];
+      if (numStr.length < 6) {
+        return numStr;
+      } else if (numStr.length >= 6 && numStr.length <= 8) {
+        const decimal = numStr.substring(
+          numStr.length - 4,
+          numStr.length - 4 + 2,
+        );
+
+        return parseFloat(((num / 10000) | 0) + "." + decimal) + "万";
+      } else if (numStr.length > 8) {
+        const decimal = numStr.substring(
+          numStr.length - 8,
+          numStr.length - 8 + 2,
+        );
+
+        return parseFloat(((num / 100000000) | 0) + "." + decimal) + "亿";
+      }
+    });
+
+    return {
+      playCount,
+    };
   },
 });
