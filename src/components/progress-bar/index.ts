ddffa2af98362eb1a42a60055146e6ac8611c955
unknown <xingguang64@yeah.net>
2024-12-26 18:02:31 +0800

    进度条组件progress-bar页面布局（2）

--- src/components/progress-bar/index.ts
+++ src/components/progress-bar/index.ts
@@ -1,7 +1,10 @@
 import { defineComponent, onReady, reactive, ref } from "@vue-mini/core";
 
 defineComponent({
-  setup() {
+  setup(props, ctx) {
+    let movableAreaWidth = 0;
+    let movableViewWidth = 0;
+    const backgroundAudioManager = wx.getBackgroundAudioManager();
     // 音乐播放时间
     const showTime = reactive({
       currentTime: "00:00",
@@ -12,26 +15,83 @@ defineComponent({
     // 进度条移动距离
     const progress = ref(0);
 
+    onReady(() => {
+      getMovableDis();
+      bindBGMEvents();
+    });
+
+    function getMovableDis() {
+      const q = ctx.createSelectorQuery();
+      q.select(".movable-area").boundingClientRect();
+      q.select(".movable-view").boundingClientRect();
+      q.exec((rect: { width: number }[]) => {
+        movableAreaWidth = rect[0].width;
+        movableViewWidth = rect[1].width;
+        console.log(movableAreaWidth, movableViewWidth);
+      });
+    }
+
+    function bindBGMEvents() {
+      backgroundAudioManager.onPlay(() => {});
+
+      backgroundAudioManager.onStop(() => {});
+
+      backgroundAudioManager.onPause(() => {});
+
+      backgroundAudioManager.onWaiting(() => {});
+
+      backgroundAudioManager.onCanplay(() => {
+        console.log(backgroundAudioManager.duration);
+        if (typeof backgroundAudioManager.duration != "undefined") {
+          setTime();
+        } else {
+          setTimeout(() => {
+            setTime();
+          }, 1000);
+        }
+      });
+
+      backgroundAudioManager.onTimeUpdate(() => {});
+
+      backgroundAudioManager.onEnded(() => {});
+
+      backgroundAudioManager.onError((res) => {
+        console.error(res.errMsg);
+        void wx.showToast({
+          title: "错误" + res.errMsg,
+        });
+      });
+    }
+
+    function setTime() {
+      const duration = backgroundAudioManager.duration;
+      const durationFmt = dateFormat(duration);
+      console.log(durationFmt);
+      Object.assign(showTime, {
+        totalTime: durationFmt.min + ":" + durationFmt.sec,
+      });
+    }
+
+    // 格式化时间
+    function dateFormat(sec: number) {
+      const min = Math.floor(sec / 60);
+      sec = Math.floor(sec % 60);
+      return {
+        min: parse0(min),
+        sec: parse0(sec),
+      };
+    }
+
+    // 补零
+    function parse0(sec: number) {
+      return sec < 10 ? "0" + sec : sec;
+    }
+
     return {
       // data
       showTime,
       movableDis,
       progress,
     };
-  },
-  lifetimes: {
-    ready() {
-      this._getMovableDis();
-    },
-  },
-  methods: {
-    _getMovableDis() {
-      const query = this.createSelectorQuery();
-      query.select(".movable-area").boundingClientRect();
-      query.select(".movable-view").boundingClientRect();
-      query.exec((res) => {
-        console.log(res);
-      });
-    },
   },
 });
