9759e683d6dcfb085716fc711f120b3a1dc6131e
xing-6464 <xingguang64@yeah.net>
2025-02-24 16:50:26 +0800

    选择图片业务逻辑

--- src/pages/blogEdit/index.ts
+++ src/pages/blogEdit/index.ts
@@ -4,7 +4,10 @@ definePage((options) => {
   console.log(options)
   const wordsNum = ref('0')
   const footerBottom = ref(0)
+  const images = ref<WechatMiniprogram.MediaFile[]>([])
+  const selectPhoto = ref(true)
   const MAX_WORDS = 140 // 微博140字限制
+  const MAX_IMAGE_NUM = 9 // 微博图片最大数量
 
   function onInput(e: { detail: { value: string } }) {
     const _wordsNum = e.detail.value.length
@@ -18,6 +21,36 @@ definePage((options) => {
   function onFocus(e: { detail: { height: number } }) {
     footerBottom.value = e.detail.height
   }
+
+  function onChooseImage() {
+    let max = MAX_IMAGE_NUM - images.value.length
+    wx.chooseMedia({
+      count: max,
+      mediaType: ['image'],
+      sizeType: ['original', 'compressed'],
+      sourceType: ['album', 'camera'],
+      success: (res) => {
+        images.value.push(...res.tempFiles) // 添加数组元素
+        max = MAX_IMAGE_NUM - images.value.length
+        selectPhoto.value = max <= 0 ? false : true
+      },
+    })
+  }
+
+  function onDeleteImage(e: { target: { dataset: { index: number } } }) {
+    images.value.splice(e.target.dataset.index, 1) // 删除数组元素
+    if (images.value.length < MAX_IMAGE_NUM) {
+      selectPhoto.value = true
+    }
+  }
+
+  function onPreviewImage(e: { target: { dataset: { imgsrc: string } } }) {
+    void wx.previewImage({
+      urls: images.value.map((item) => item.tempFilePath),
+      current: e.target.dataset.imgsrc,
+    })
+  }
+
   function onBlur() {
     footerBottom.value = 0
   }
@@ -25,8 +58,13 @@ definePage((options) => {
   return {
     wordsNum,
     footerBottom,
+    selectPhoto,
+    images,
+    onDeleteImage,
+    onPreviewImage,
     onInput,
     onFocus,
     onBlur,
+    onChooseImage,
   }
 })
